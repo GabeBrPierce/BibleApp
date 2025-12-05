2c5b3161e109ea9a4b81bc834f235aad67fcd86f
Flip The Developer <flipthedeveloper@gmail.com>
2025-12-05 08:33:06 -0700

    feat:t rename reader to read

--- src/js/page/index.js
+++ src/js/page/index.js
@@ -1,11 +1,53 @@
 window.addEventListener("load", () => {
-  Menu.init(".js-menu", (id) => console.log("Selected", id));
+  // Initialize the menu on this page
+  Menu.init(".js-menu", onMenuSelect);
 
+  // Softkeys for this page
   Softkeys.set("", "Select", "");
 
+  // Key navigation for this page
   KeyRouter.setHandlers({
     ArrowUp: () => Menu.move(-1),
     ArrowDown: () => Menu.move(1),
-    Enter: () => Menu.select(),
+    Enter: () => Menu.select()
   });
 });
+
+// -------------------------------------------
+// Page-specific actions
+// -------------------------------------------
+function onMenuSelect(id) {
+  switch (id) {
+    case "continueReading":
+      goTo("/page/read.html");
+      break;
+
+    case "continueFromBookmark":
+      goTo("/page/bookmarks.html");
+      break;
+
+    case "searchAddress":
+      goTo("/page/search-address.html");
+      break;
+
+    case "searchKeyword":
+      goTo("/page/search-keyword.html");
+      break;
+
+    case "manageBookmarks":
+      goTo("/page/manage-bookmarks.html");
+      break;
+
+    case "manageHighlights":
+      goTo("/page/manage-highlights.html");
+      break;
+
+    case "manageNotes":
+      goTo("/page/manage-notes.html");
+      break;
+  }
+}
+
+function goTo(path) {
+  window.location.href = path;
+}
